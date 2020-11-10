5b49818fa74309ea83dc80509b468f66ddbe5bfa
Aaquib Equbal <armanaaquibequbal@gmail.com>
2020-11-10 08:42:04 +0530

    Add prompt and right_prompt for fish

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,24 @@
+function fish_prompt
+    set -l cmd_status $status
+
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+    end
+
+    echo -n (set_color blue)(prompt_pwd)' '
+
+    set_color -o
+    if test "$USER" = 'root'
+        echo -n (set_color red)'# '
+    end
+
+    echo -n (set_color yellow)'❯'
+    if test $cmd_status -eq 0
+        set_color green
+    else
+        set_color red
+    end
+    echo -n '❯ '
+
+    set_color normal
+end
